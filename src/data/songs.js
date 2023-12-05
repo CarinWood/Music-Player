9ec349b97e01903a71d353e40cd27d3236c26eda
CarinWood <carin.wood.85@gmail.com>
2023-12-05 10:58:02 +0100

    first commit

---
+++ src/data/songs.js
@@ -0,0 +1,69 @@
+import image1 from '../assets/images/SummertimeSadness.png'
+import image2 from '../assets/images/LanaDelRey-HighByTheBeach.png'
+import image3 from "../assets/images/LanaBlueJeans.png"
+import image4 from "../assets/images/cola.png"
+import image5 from "../assets/images/Lana_Del_Rey_-_West_Coast.png"
+import image6 from "../assets/images/artdeco.png"
+import image7 from "../assets/images/Young_and_Beautiful_cover_art.jpg"
+
+import summer from "../assets/audio/summer.mp3"
+import beach from "../assets/audio/beach.mp3"
+import cola from "../assets/audio/cola.mp3"
+import deco from "../assets/audio/deco.mp3"
+import jeans from "../assets/audio/jeans.mp3"
+import young from "../assets/audio/young.mp3"
+import coast from "../assets/audio/coast.mp3"
+
+export const songs = [
+{
+    id: 1,
+    title: 'Summertime Sadness',
+    artist: 'Lana Del Rey',
+    img: image1,
+    audio: summer,
+},
+ {
+    id: 2,
+    title: "High by the Beach",
+    artist: "Lana Del Rey",
+    img: image2,
+    audio: beach
+},
+{
+    id: 3,
+    title: "Blue Jeans",
+    artist: "Lana Del Rey",
+    img: image3, 
+    audio: jeans
+},
+
+{
+    id: 4,
+    title: "Cola",
+    artist: "Lana Del Rey",
+    img: image4,
+    audio: cola,
+},
+{
+    id: 5,
+    title: "West Coast",
+    artist: "Lana Del Rey",
+    img: image5,  
+    audio: coast
+}, 
+
+{
+    id: 6,
+    title: "Art Deco",
+    artist: "Lana Del Rey",
+    img: image6,
+    audio: deco,
+},
+{
+    id: 7,
+    title: "Young and beautiful",
+    artist: "Lana Del Rey",
+    img: image7, 
+    audio: young,
+} 
+]
